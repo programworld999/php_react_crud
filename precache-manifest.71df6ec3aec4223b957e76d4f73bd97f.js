fc178a91be5626ebcd93a03350d64785c06592ef
programworld999 <programworld999@gmail.com>
2019-05-23 00:15:47 +0530

    f

---
+++ precache-manifest.71df6ec3aec4223b957e76d4f73bd97f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "39a96f47a151a5195205",
+    "url": "/php_react_crud/static/css/main.03431e8d.chunk.css"
+  },
+  {
+    "revision": "39a96f47a151a5195205",
+    "url": "/php_react_crud/static/js/main.1c3e4b41.chunk.js"
+  },
+  {
+    "revision": "e5d90a7b89142cf136c6",
+    "url": "/php_react_crud/static/js/runtime~main.9f89430b.js"
+  },
+  {
+    "revision": "0cff1fb3a0ea2e1dbe10",
+    "url": "/php_react_crud/static/css/2.ebf5c81e.chunk.css"
+  },
+  {
+    "revision": "0cff1fb3a0ea2e1dbe10",
+    "url": "/php_react_crud/static/js/2.5fbd17de.chunk.js"
+  },
+  {
+    "revision": "b10b0c6b38a32f7f0690a488bc3c8ab7",
+    "url": "/php_react_crud/index.html"
+  }
+];
